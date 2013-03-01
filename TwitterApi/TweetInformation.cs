b162609e01b13d506e9a4e0837ac90fd0398c86e
Yong Heng <Yongheng@live.com.sg>
2013-03-02 02:31:36 +0800

    first

---
+++ TwitterApi/TweetInformation.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Misterhex
+{
+    public class TweetInformation : IEqualityComparer<TweetInformation>
+    {
+        public string Content { get; set; }
+        public string Name { get; set; }
+        public string TimeZone { get; set; }
+        public string CreatedAt { get; set; }
+        public bool IsContentContainingURL
+        {
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(this.Content) && this.Content.Contains(@"http://"))
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        public string FormattedContent
+        {
+            get
+            {
+                if (string.IsNullOrWhiteSpace(this.Content)) return this.Content;
+
+                StringBuilder sb = new StringBuilder();
+
+                foreach (var section in this.Content.Split(' '))
+                {
+                    if (section.StartsWith("http://"))
+                    {
+                        sb.Append(@"<a href='" + section + "'></a> ");
+                    }
+                    else
+                    {
+                        sb.Append(section + ' ');
+                    }
+                }
+                var result = sb.ToString().TrimEnd();
+                return result;
+
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var property in this.GetType().GetProperties().ToList())
+            {
+                sb.AppendLine(string.Format("{0} : {1}", property.Name, property.GetValue(this, null)));
+            }
+
+            return sb.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            string content = this.Content != null ? this.Content : string.Empty;
+            string name = this.Name != null ? this.Name : string.Empty;
+            string timezone = this.TimeZone != null ? this.TimeZone : string.Empty;
+            string createdAt = this.CreatedAt != null ? this.CreatedAt : string.Empty;
+
+            int hash = 17;
+            hash = hash * 31 + content.GetHashCode();
+            hash = hash * 31 + name.GetHashCode();
+            hash = hash * 31 + timezone.GetHashCode();
+            hash = hash * 31 + createdAt.GetHashCode();
+            return hash;
+        }
+
+        public bool Equals(TweetInformation x, TweetInformation y)
+        {
+            return x.GetHashCode() == y.GetHashCode();
+        }
+
+        public int GetHashCode(TweetInformation obj)
+        {
+            return obj.GetHashCode();
+        }
+
+        public string ToJSON()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(this);
+        }
+    }
+}
---
+++ TwitterApi/TwitterApi.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Misterhex
+{
+    public class TwitterApi
+    {
+        private readonly IEnumerable<KeyValuePair<string, string>> _paramsWithoutSignature;
+
+        public TwitterApi()
+        {
+            _paramsWithoutSignature = GetParametersWithoutSignature();
+        }
+
+        public virtual string Request(string url)
+        {
+            var authorizationHeader = GetAuthorizationHeader(url);
+
+            WebRequest webRequest = HttpWebRequest.Create(url);
+            webRequest.Headers.Add(HttpRequestHeader.Authorization, authorizationHeader);
+
+            var webResponse = webRequest.GetResponse();
+            StreamReader streamReader = new StreamReader(webResponse.GetResponseStream());
+            var responseString = streamReader.ReadToEnd();
+            return responseString;
+        }
+
+        private long SecondsSinceEpoch()
+        {
+            TimeSpan t = DateTime.UtcNow - new DateTime(1970, 1, 1);
+            return (int)t.TotalSeconds;
+        }
+
+        private string GetAuthorizationHeader(string url)
+        {
+            List<KeyValuePair<string, string>> paramsWithoutSignatureList = new List<KeyValuePair<string, string>>(this._paramsWithoutSignature);
+
+            string signature = CreateSignature(HttpMethod.GET, url);
+            paramsWithoutSignatureList.Add(new KeyValuePair<string, string>("oauth_signature", signature));
+
+            var paramsWithSignatureList = paramsWithoutSignatureList.OrderBy(i => i.Key).ToList();
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append("OAuth ");
+
+            foreach (var keyValue in paramsWithSignatureList)
+            {
+                sb.Append(keyValue.Key.UrlEncodeToUpper());
+                sb.Append("=");
+                sb.Append('"');
+                sb.Append(keyValue.Value.UrlEncodeToUpper());
+                sb.Append('"');
+                sb.Append(", ");
+            }
+
+            string result = sb.ToString().TrimEnd(',', ' ');
+            return result;
+        }
+
+        private List<KeyValuePair<string, string>> GetParametersWithoutSignature()
+        {
+            string nounce = GenerateNounce();
+            string timestamp = SecondsSinceEpoch().ToString();
+
+            List<KeyValuePair<string, string>> keyValues = new List<KeyValuePair<string, string>>() 
+            {
+                new KeyValuePair<string,string>("oauth_consumer_key","YasidTt6NvhVRkzeQoTXw"),
+                new KeyValuePair<string,string>("oauth_nonce",nounce),
+                new KeyValuePair<string,string>("oauth_signature_method","HMAC-SHA1"),
+                new KeyValuePair<string,string>("oauth_timestamp",timestamp),
+                new KeyValuePair<string,string>("oauth_token","140354657-EKsNXJrBwRcv73W4k3fn3Uf32lRDs6o99h9vo91h"),
+                new KeyValuePair<string,string>("oauth_version","1.0")
+            };
+            return keyValues;
+        }
+
+        private string GenerateNounce()
+        {
+            string s = Guid.NewGuid().ToString();
+            s = s.Replace("-", string.Empty);
+            byte[] bytes = ASCIIEncoding.ASCII.GetBytes(s);
+            string result = Convert.ToBase64String(bytes);
+            result = new string(result.Take(32).ToArray());
+            return result;
+        }
+
+        private List<KeyValuePair<string, string>> ParseUrlParams(string url)
+        {
+            int index = url.IndexOf("?") + 1;
+            string toParse = new string(url.Skip(index).ToArray());
+
+            List<KeyValuePair<string, string>> result = new List<KeyValuePair<string, string>>();
+            foreach (var keyValueString in toParse.Split('&'))
+            {
+                var keyAndValue = keyValueString.Split('=').ToList();
+                if (keyAndValue.Count == 2)
+                {
+                    result.Add(new KeyValuePair<string, string>(keyAndValue.ElementAt(0), keyAndValue.ElementAt(1)));
+                }
+            }
+
+            return result;
+        }
+
+        private string CreateSignatureParameterString(string url)
+        {
+            var urlParams = ParseUrlParams(url);
+
+            var sortedByKey = this._paramsWithoutSignature.Concat(urlParams).OrderBy(i => i.Key);
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var param in sortedByKey)
+            {
+                sb.Append(param.Key.UrlEncodeToUpper() + "=" + param.Value.UrlEncodeToUpper());
+                sb.Append("&");
+            }
+            var result = sb.ToString().TrimEnd('&');
+            return result;
+        }
+
+        private string CreateSignatureBaseString(HttpMethod method, string url)
+        {
+            string urlMain = url.IndexOf('?') != -1 ? new string(url.Take(url.IndexOf('?')).ToArray()) : url;
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(method.ToString().ToUpper());
+            sb.Append("&");
+            sb.Append(urlMain.UrlEncodeToUpper());
+            sb.Append("&");
+            sb.Append(CreateSignatureParameterString(url).UrlEncodeToUpper());
+            var result = sb.ToString();
+
+            return result;
+        }
+
+        private string CreateSigningKeyString()
+        {
+            string consumerSecret = "lc3nWO73RN5MOQ1FEn2Pgbh0rMSlXOtFneZH3c69g";
+            string tokenSecret = "MUFMkNhpPH8e7PbaSbJ7Ys7AlfK1fDHv76TvT4Ojk";
+            string signingKey = consumerSecret.UrlEncodeToUpper() + "&" + tokenSecret.UrlEncodeToUpper();
+            return signingKey;
+        }
+
+        private string CreateSignature(HttpMethod method, string url)
+        {
+            string signingKey = CreateSigningKeyString();
+            string baseString = CreateSignatureBaseString(method, url);
+
+            byte[] key = Encoding.ASCII.GetBytes((signingKey));
+            byte[] signatureBaseString = Encoding.ASCII.GetBytes((baseString));
+            HMACSHA1 hmacsha1 = new HMACSHA1(key);
+            byte[] hash = hmacsha1.ComputeHash(signatureBaseString);
+            string signature = Convert.ToBase64String(hash);
+
+            return signature;
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static string UrlEncodeToUpper(this string s)
+        {
+            string encoded = HttpUtility.UrlEncode(s);
+
+            List<char> newForm = new List<char>();
+
+            int counter = 0;
+            foreach (var c in encoded)
+            {
+                if (c == '%')
+                {
+                    newForm.Add(c);
+                    counter = 2;
+                    continue;
+                }
+
+                if (counter > 0)
+                {
+                    newForm.Add(c.ToString().ToUpper().ToArray().First());
+
+                }
+                else
+                {
+                    newForm.Add(c);
+                }
+                counter--;
+            }
+
+            string result = new string(newForm.ToArray());
+            return result;
+        }
+
+    }
+
+    public enum HttpMethod
+    {
+        POST,
+        GET
+    }
+}
